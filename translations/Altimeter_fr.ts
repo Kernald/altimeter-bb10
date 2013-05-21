b4845deaab3bab161375fdbc20904a84d3f856d2
Marc Plano-Lesay <marc.planolesay@gmail.com>
2013-05-21 17:03:18 +0200

    French translation

--- translations/Altimeter_fr.ts
+++ translations/Altimeter_fr.ts
@@ -5,58 +5,58 @@
     <name>ApplicationUI</name>
     <message utf8="true">
         <source>Latitude: %1°</source>
-        <translation type="unfinished"></translation>
+        <translation>Latitude : %1 °</translation>
     </message>
     <message utf8="true">
         <source>Longitude: %1°</source>
-        <translation type="unfinished"></translation>
+        <translation>Longitude : %1 °</translation>
     </message>
     <message>
         <source>Waiting for GPS...</source>
-        <translation type="unfinished"></translation>
+        <translation>En attente du GPS…</translation>
     </message>
     <message>
         <source>meters</source>
-        <translation type="unfinished"></translation>
+        <translation>mètres</translation>
     </message>
     <message>
         <source>feet</source>
-        <translation type="unfinished"></translation>
+        <translation>pieds</translation>
     </message>
     <message>
         <source>yards</source>
-        <translation type="unfinished"></translation>
+        <translation>yards</translation>
     </message>
     <message>
         <source>Altitude: %1 %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Altitude : %1 %2</translation>
     </message>
 </context>
 <context>
     <name>settings</name>
     <message>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Paramètres</translation>
     </message>
     <message>
         <source>Unit</source>
-        <translation type="unfinished"></translation>
+        <translation>Unité</translation>
     </message>
     <message>
         <source>Meters</source>
-        <translation type="unfinished"></translation>
+        <translation>Mètres</translation>
     </message>
     <message>
         <source>Feet</source>
-        <translation type="unfinished"></translation>
+        <translation>Pieds</translation>
     </message>
     <message>
         <source>Yards</source>
-        <translation type="unfinished"></translation>
+        <translation>Yards</translation>
     </message>
     <message>
         <source>Top altitude</source>
-        <translation type="unfinished"></translation>
+        <translation>Altitude maximale</translation>
     </message>
 </context>
 </TS>
